4e055ca78991dd45c902044a5d6b4b40d7314e4e
shaileshsmartworking <30897581+shailesh941@users.noreply.github.com>
2026-04-27 15:37:38 +0530

    add automation deployment CGP
    add automation deployment CGP

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: "nest-api",
-      script: "dist/main.js",
+      name: "nest-app",
       cwd: "/var/www/app/backend",
+      script: "dist/main.js",
+      instances: 1,
+      exec_mode: "fork",
       env: {
         NODE_ENV: "production",
-        PORT: 3001            // Keeps the API on a separate port
+        PORT: 3001
       }
     }
   ]
